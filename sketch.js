c56831a8fc9f94999eb6f1d352d6b695f800e713
shivsm145 <83943242+shivsm145@users.noreply.github.com>
2021-07-01 11:21:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload()
+{
+  getBackgroundImg()
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    if(backgroundImg)
+    background(backgroundImg);
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+
+   var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+   var responseJSON = await response.json();
+   var datetime = responseJSON.datetime;
+   var hour =  datetime.slice(11,13);
+   console.log(hour);
+   if(hour>=04 && hour<=06)
+   {
+     bg = "sunrise1.png";
+   }
+   else if(hour>=06 && hour<=08)
+   {
+    bg = "sunrise2.png";
+   }
+   else if(hour>=08 && hour<=12)
+   {
+    bg = "sunrise3.png";
+   }
+   else if(hour>=12 && hour<=14)
+   {
+    bg = "sunrise5.png";
+   }
+   else if(hour>=14 && hour<=16)
+   {
+    bg = "sunrise6.png";
+   }
+   else if(hour>=16 && hour<=19)
+   {
+    bg = "sunset7.png";
+   }
+   else if(hour>=19 && hour<=20)
+   {
+    bg = "sunset8.png";
+   }
+   else if(hour>=20 && hour<=23)
+   {
+    bg = "sunset10.png";
+   }
+   else if(hour>=23 && hour==0)
+   {
+    bg = "sunset11.png";
+   }
+   else if(hour>=0 && hour<=03)
+   {
+    bg = "sunset12.png";
+   }
+   else
+   {
+     //bg = "sunset12.png";
+   }
+   
+    backgroundImg = loadImage(bg);
+
+}
